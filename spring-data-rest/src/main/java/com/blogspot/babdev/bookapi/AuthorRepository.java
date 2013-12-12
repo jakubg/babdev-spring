561d347955c06e583b96e561a6fafd28a4e9326d
michal.amerek <michal.amerek@schibsted.pl>
2013-12-12 14:56:33 +0100

    Book and Author removed

--- spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogspot.babdev.bookapi;
-
-import com.blogspot.babdev.bookapi.model.Author;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.repository.annotation.RestResource;
-
-@RestResource(path = "authors", rel = "authors")
-public interface AuthorRepository extends PagingAndSortingRepository<Author, Integer> {
-}
--- spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogspot.babdev.bookapi;
-
-import com.blogspot.babdev.bookapi.model.Book;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.repository.annotation.RestResource;
-
-@RestResource(path = "books", rel = "books")
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
-}
