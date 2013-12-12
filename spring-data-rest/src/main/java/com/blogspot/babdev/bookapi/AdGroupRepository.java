e862b6f04076f020f598906c79d1c102e208e613
jakubg <jakub.grzechynka@schibsted.pl>
2013-12-12 15:03:45 +0100

    AdGroup and AdGroupRepository enhancements

--- spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/AdGroupRepository.java
+++ spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/AdGroupRepository.java
@@ -1,11 +1,19 @@
 package com.blogspot.babdev.bookapi;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.repository.annotation.RestResource;
 
 import com.blogspot.babdev.bookapi.model.AdGroup;
+import com.blogspot.babdev.bookapi.model.AdList;
 
 @RestResource(path = "adGroups", rel = "adGroups")
 public interface AdGroupRepository extends PagingAndSortingRepository<AdGroup, Long>  {
+
+	public List<AdList> findByName(@Param("name") String name);
+
+	public List<AdList> findByActive(@Param("active") boolean active);
 
 }
--- spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/model/AdGroup.java
+++ spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/model/AdGroup.java
@@ -1,11 +1,19 @@
 package com.blogspot.babdev.bookapi.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "T_AD_GROUP")
@@ -16,12 +24,46 @@ public class AdGroup {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ad_group_id", nullable = false)
     private Integer id;
-    private String name;
-    private Boolean active = new Boolean(true);
-    //private List<Ad> ads;
     
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+    
+    @Column(name = "active", unique = false, nullable = true)
+    private Boolean active = new Boolean(true);
+    
+    @ManyToMany(fetch = FetchType.LAZY)
+    // TODO get rid of such filtering on entities
+    @Where(clause = "active='1'")
+    @JoinTable(name = "T_AD_GROUP_T_AD", joinColumns = { @JoinColumn(name = "ad_group_id") }, inverseJoinColumns = { @JoinColumn(name = "ad_id") })
+    private List<Ad> ads;
+    
+    
+	public List<Ad> getAds() {
+		return ads;
+	}
+
+	public void setAds(List<Ad> ads) {
+		this.ads = ads;
+	}
+
 	public Integer getId() {
 		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
 	}
 
 	public void setId(Integer id) {
