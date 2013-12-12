c206ee2c7ab460c571af3f1d42d873367c3bdbb7
jakubg <jakub.grzechynka@schibsted.pl>
2013-12-12 13:56:04 +0100

    findByActive

--- spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/AdRepository.java
+++ spring-data-rest/src/main/java/com/blogspot/babdev/bookapi/AdRepository.java
@@ -3,6 +3,7 @@ package com.blogspot.babdev.bookapi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.repository.annotation.RestResource;
 
 import com.blogspot.babdev.bookapi.model.Ad;
@@ -11,5 +12,5 @@ import com.blogspot.babdev.bookapi.model.AdList;
 @RestResource(path = "ads", rel = "ads")
 public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
 	
-	Page<AdList> findByActive(boolean active, Pageable pageable);
+	Page<AdList> findByActive(@Param("active") boolean active, Pageable pageable);
 }
