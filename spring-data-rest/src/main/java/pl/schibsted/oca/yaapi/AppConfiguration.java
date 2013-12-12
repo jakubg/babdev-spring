691650102f8d64c238b64779b1b4787927f0a99a
jakubg <jakub.grzechynka@schibsted.pl>
2013-12-12 17:17:03 +0100

    Removed warnings for presentation purposes

--- spring-data-rest/src/main/java/pl/schibsted/oca/yaapi/AppConfiguration.java
+++ spring-data-rest/src/main/java/pl/schibsted/oca/yaapi/AppConfiguration.java
@@ -1,12 +1,12 @@
 package pl.schibsted.oca.yaapi;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -14,9 +14,6 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories
